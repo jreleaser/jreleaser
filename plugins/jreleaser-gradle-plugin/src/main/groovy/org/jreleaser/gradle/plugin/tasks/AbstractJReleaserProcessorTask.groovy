08586b8fe7b5a7e6b6db7ebd8d80d96997df5d74
Andres Almiray <aalmiray@gmail.com>
2021-03-19 15:55:24 +0100

    Fix license headers

--- plugins/jreleaser-gradle-plugin/src/main/groovy/org/jreleaser/gradle/plugin/tasks/AbstractJReleaserProcessorTask.groovy
+++ plugins/jreleaser-gradle-plugin/src/main/groovy/org/jreleaser/gradle/plugin/tasks/AbstractJReleaserProcessorTask.groovy
@@ -3,7 +3,7 @@
  *
  * Copyright 2020-2021 Andres Almiray.
  *
- * Licensed under the Apache License, Version 2.0 (the "License")
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
