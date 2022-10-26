04626ac89c3eb36582d8f8590d58f6c1a3cd790c
Andres Almiray <aalmiray@gmail.com>
2022-10-26 09:34:54 +0200

    build: Relocate extension impl classes to internal package

--- core/jreleaser-engine/src/main/java/org/jreleaser/extensions/internal/mustache/DefaultMustacheExtension.java
+++ core/jreleaser-engine/src/main/java/org/jreleaser/extensions/internal/mustache/DefaultMustacheExtension.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jreleaser.extensions.impl.mustache;
+package org.jreleaser.extensions.internal.mustache;
 
 import org.jreleaser.extensions.api.Extension;
 import org.jreleaser.extensions.api.ExtensionPoint;
--- core/jreleaser-engine/src/main/java/org/jreleaser/extensions/internal/mustache/DefaultMustacheExtensionPoint.java
+++ core/jreleaser-engine/src/main/java/org/jreleaser/extensions/internal/mustache/DefaultMustacheExtensionPoint.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jreleaser.extensions.impl.mustache;
+package org.jreleaser.extensions.internal.mustache;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
