b3624ef4194f1a9869b3d13698b849772b52af40
Andres Almiray <aalmiray@gmail.com>
2022-12-14 13:12:21 +0100

    test: Add some unit tests to jreleaser-utils

---
+++ api/jreleaser-utils/src/test/java/org/jreleaser/util/CollectionUtilsTest.java
@@ -0,0 +1,165 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020-2022 The JReleaser authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jreleaser.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollectionUtilsTest {
+    @Test
+    public void testMapBuilderAdDelegate() {
+        // given:
+        Map<String, String> delegate = new LinkedHashMap<>();
+        Map<String, String> map = CollectionUtils.map(delegate);
+
+        // when:
+        map.put("key", "value");
+
+        // then:
+        assertEquals(1, map.size());
+        assertEquals(map.keySet(), delegate.keySet());
+        assertEquals(map.values(), delegate.values());
+        assertEquals(map.containsKey("key"), delegate.containsKey("key"));
+        assertEquals(map.containsValue("value"), delegate.containsValue("value"));
+
+        // when:
+        map.remove("key");
+
+        // then:
+        assertEquals(0, delegate.size());
+
+        // when:
+        map.put("key", "value");
+        assertEquals(1, delegate.size());
+        map.clear();
+
+        // then:
+        assertEquals(0, delegate.size());
+
+        assertEquals(map.hashCode(), delegate.hashCode());
+    }
+
+    @Test
+    public void testListBuilderAndDelegate() {
+        // given:
+        List<Object> delegate = new ArrayList<>();
+        CollectionUtils.ListBuilder<Object> list = CollectionUtils.list(delegate);
+
+        // when:
+        list.add(2);
+        list.add(0, 0);
+        list.set(1, 1);
+        list.add("foo");
+
+        // then:
+        assertEquals(3, delegate.size());
+        assertEquals(asList(0, 1, "foo"), delegate);
+
+        // when:
+        list.remove("foo");
+        // then:
+        assert delegate.equals(new ArrayList<Integer>(asList(0, 1)));
+
+        // when:
+        list.clear();
+        // then:
+        assertEquals(0, delegate.size());
+
+        assert list.hashCode() == delegate.hashCode();
+
+        // when:
+        list.e(1);
+        // then:
+        assertEquals(list.size(), delegate.size());
+        assertEquals(list.contains(1), delegate.contains(1));
+        assertTrue(() -> list.containsAll(asList(1)));
+    }
+
+    @Test
+    public void testSetBuilderAndDelegate() {
+        // given:
+        Set<Object> delegate = new LinkedHashSet<>();
+        CollectionUtils.SetBuilder<Object> set = CollectionUtils.set(delegate);
+
+        // when:
+        set.add(0);
+        set.add(1);
+        set.add("foo");
+
+        // then:
+        assertEquals(3, delegate.size());
+
+        // when:
+        set.remove("foo");
+        // then:
+        assertEquals(2, delegate.size());
+        assertTrue(() -> delegate.contains(0));
+        assertTrue(() -> delegate.contains(1));
+
+        // when:
+        set.clear();
+        // then:
+        assertEquals(0, delegate.size());
+
+        assert set.hashCode() == delegate.hashCode();
+
+        // when:
+        set.e(1);
+        // then:
+        assertEquals(set.size(), delegate.size());
+        assertEquals(set.contains(1), delegate.contains(1));
+        assertTrue(() -> set.containsAll(asList(1)));
+    }
+
+    @Test
+    public void testToPropertiesDeep() {
+        // given:
+        Map<String, Object> map1 = new LinkedHashMap<>(5);
+        map1.put("singleKey", "singleValue");
+        map1.put("key.string", "string");
+        map1.put("key.boolean", true);
+        map1.put("key.list", asList(1, 2, 3));
+        LinkedHashMap<String, Integer> map2 = new LinkedHashMap<String, Integer>(2);
+        map2.put("one", 1);
+        map2.put("two", 2);
+        map1.put("key.map", map2);
+
+        // when:
+        Properties props = CollectionUtils.toPropertiesDeep(map1);
+
+        // then:
+        assertEquals(6, props.keySet().size());
+        assertEquals("singleValue", props.getProperty("singleKey"));
+        assertEquals("string", props.getProperty("key.string"));
+        assertEquals(true, props.get("key.boolean"));
+        assertEquals(asList(1, 2, 3), props.get("key.list"));
+        assertEquals(1, props.get("key.map.one"));
+        assertEquals(2, props.get("key.map.two"));
+    }
+}
---
+++ api/jreleaser-utils/src/test/java/org/jreleaser/util/StringUtilsTest.java
@@ -0,0 +1,182 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020-2022 The JReleaser authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jreleaser.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringUtilsTest {
+    @Test
+    public void testCapitalize() {
+        assertEquals("", StringUtils.capitalize(""));
+        assertEquals(" ", StringUtils.capitalize(" "));
+        assertNull(StringUtils.capitalize(null));
+        assertEquals("A", StringUtils.capitalize("a"));
+        assertEquals("Jreleaser", StringUtils.capitalize("jreleaser"));
+    }
+
+    @Test
+    public void testUncapitalize() {
+        assertEquals("", StringUtils.uncapitalize(""));
+        assertEquals(" ", StringUtils.uncapitalize(" "));
+        assertNull(StringUtils.uncapitalize(null));
+        assertEquals("a", StringUtils.uncapitalize("A"));
+        assertEquals("jreleaser", StringUtils.uncapitalize("Jreleaser"));
+    }
+
+    @Test
+    public void testGetSetterName() {
+        assertEquals("setA", StringUtils.getSetterName("a"));
+        assertEquals("setJreleaser", StringUtils.getSetterName("jreleaser"));
+    }
+
+    @Test
+    public void testGetGetterName() {
+        assertEquals("getA", StringUtils.getGetterName("a"));
+        assertEquals("getJreleaser", StringUtils.getGetterName("jreleaser"));
+    }
+
+    @Test
+    public void testGetClassName() {
+        assertEquals("PersonController", StringUtils.getClassName("person", "Controller"));
+        assertEquals("Person", StringUtils.getClassName("person", ""));
+    }
+
+    @Test
+    public void testGetPropertyName() {
+        assertEquals("foo", StringUtils.getPropertyName(Foo.class));
+    }
+
+    @Test
+    public void testGetPropertyNameRepresentation() {
+        assertEquals("foo", StringUtils.getPropertyName("foo"));
+        assertEquals("foo", StringUtils.getPropertyName("Foo"));
+        assertEquals("foo", StringUtils.getPropertyName("jreleaser.util.Foo"));
+        assertEquals("fooBar", StringUtils.getPropertyName("Foo Bar"));
+    }
+
+    @Test
+    public void testGetShortName() {
+        assertEquals("Foo", StringUtils.getShortName("jreleaser.util.Foo"));
+        assertEquals("Foo", StringUtils.getShortName(Foo.class));
+    }
+
+    @Test
+    public void testGetClassNameRepresentation() {
+        assertEquals("MyClass", StringUtils.getClassNameRepresentation("my-class"));
+        assertEquals("MyClass", StringUtils.getClassNameRepresentation("MyClass"));
+        assertEquals("F", StringUtils.getClassNameRepresentation(".f"));
+        assertEquals("AB", StringUtils.getClassNameRepresentation(".a.b"));
+        assertEquals("AlphaBakerCharlie", StringUtils.getClassNameRepresentation(".alpha.baker.charlie"));
+    }
+
+    @Test
+    public void testGetNaturalName() {
+        assertEquals("First Name", StringUtils.getNaturalName("firstName"));
+        assertEquals("URL", StringUtils.getNaturalName("URL"));
+        assertEquals("Local URL", StringUtils.getNaturalName("localURL"));
+        assertEquals("URL local", StringUtils.getNaturalName("URLlocal"));
+        assertEquals("My Domain Class", StringUtils.getNaturalName("MyDomainClass"));
+        assertEquals("My Domain Class", StringUtils.getNaturalName("com.myco.myapp.MyDomainClass"));
+    }
+
+    @Test
+    public void testGetLogicalName() {
+        assertEquals("Test", StringUtils.getLogicalName("TestController", "Controller"));
+        assertEquals("Test", StringUtils.getLogicalName("org.music.TestController", "Controller"));
+    }
+
+    @Test
+    public void testGetLogicalPropertyName() {
+        assertEquals("myFunky", StringUtils.getLogicalPropertyName("MyFunkyController", "Controller"));
+        assertEquals("HTML", StringUtils.getLogicalPropertyName("HTMLCodec", "Codec"));
+        assertEquals("payRoll", StringUtils.getLogicalPropertyName("org.something.PayRollController", "Controller"));
+    }
+
+    @Test
+    public void testGetLogicalPropertyNameForArtefactWithSingleCharacterName() {
+        assertEquals("a", StringUtils.getLogicalPropertyName("AController", "Controller"));
+        assertEquals("b", StringUtils.getLogicalPropertyName("BService", "Service"));
+    }
+
+    @Test
+    public void testGetLogicalPropertyNameForArtefactWithAllUpperCaseName() {
+        assertEquals("ABC", StringUtils.getLogicalPropertyName("ABCController", "Controller"));
+        assertEquals("BCD", StringUtils.getLogicalPropertyName("BCDService", "Service"));
+    }
+
+    @Test
+    public void testIsBlank() {
+        assertTrue(StringUtils.isBlank(null), "'null' value should count as blank.");
+        assertTrue(StringUtils.isBlank(""), "Empty string should count as blank.");
+        assertTrue(StringUtils.isBlank("  "), "Spaces should count as blank.");
+        assertTrue(StringUtils.isBlank("\t"), "A tab should count as blank.");
+        assertFalse(StringUtils.isBlank("\t  h"), "String with whitespace and non-whitespace should not count as blank.");
+        assertFalse(StringUtils.isBlank("test"), "String should not count as blank.");
+    }
+
+    @Test
+    public void testIsNotBlank() {
+        assertFalse(StringUtils.isNotBlank(null), "'null' value should count as blank.");
+        assertFalse(StringUtils.isNotBlank(""), "Empty string should count as blank.");
+        assertFalse(StringUtils.isNotBlank("  "), "Spaces should count as blank.");
+        assertFalse(StringUtils.isNotBlank("\t"), "A tab should count as blank.");
+        assertTrue(StringUtils.isNotBlank("\t  h"), "String with whitespace and non-whitespace should not count as blank.");
+        assertTrue(StringUtils.isNotBlank("test"), "String should not count as blank.");
+    }
+
+    @Test
+    public void testQuote() {
+        assertEquals(" ", StringUtils.quote(" "));
+        assertEquals("\" a\"", StringUtils.quote(" a"));
+        assertEquals("\" a \"", StringUtils.quote(" a "));
+        assertEquals("\"a \"", StringUtils.quote("a "));
+    }
+
+    @Test
+    public void testUnquote() {
+        assertEquals("", StringUtils.unquote(""));
+        assertEquals(" ", StringUtils.unquote(" "));
+        assertEquals("", StringUtils.unquote("\"\""));
+        assertEquals(" ", StringUtils.unquote("\" \""));
+        assertEquals("foo", StringUtils.unquote("\"foo\""));
+        assertEquals("", StringUtils.unquote("''"));
+        assertEquals(" ", StringUtils.unquote("' '"));
+        assertEquals("foo", StringUtils.unquote("'foo'"));
+        assertEquals("\"foo", StringUtils.unquote("\"foo"));
+        assertEquals("foo\"", StringUtils.unquote("foo\""));
+        assertEquals("'foo", StringUtils.unquote("'foo"));
+        assertEquals("foo'", StringUtils.unquote("foo'"));
+    }
+
+    @Test
+    public void testGetHyphenatedName() {
+        assertEquals("string-utils", StringUtils.getHyphenatedName(StringUtils.class));
+        assertEquals("string-utils", StringUtils.getHyphenatedName(StringUtils.class.getName()));
+    }
+
+    @Test
+    public void testGetClassNameForLowerCaseHyphenSeparatedName() {
+        assertEquals("StringUtils", StringUtils.getClassNameForLowerCaseHyphenSeparatedName("string-utils"));
+    }
+}
+
