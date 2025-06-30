ae660b7f6e5d579d21ab8a4e7551859d5029c4aa
Andres Almiray <aalmiray@gmail.com>
2025-06-30 19:34:24 +0200

    fix(gradle): Fix type accessor
    Related to #1665

--- plugins/jreleaser-gradle-plugin/src/main/groovy/org/jreleaser/gradle/plugin/internal/dsl/deploy/maven/AbstractMavenDeployer.groovy
+++ plugins/jreleaser-gradle-plugin/src/main/groovy/org/jreleaser/gradle/plugin/internal/dsl/deploy/maven/AbstractMavenDeployer.groovy
@@ -176,7 +176,7 @@ abstract class AbstractMavenDeployer implements MavenDeployer {
         if (authorization.present) deployer.authorization = authorization.get()
         Set<String> temp = new TreeSet<>()
         temp.addAll((List<String>) stagingRepositories.getOrElse([]))
-        stagingRepositories2.each { RegularFile file -> temp.add(file.asFile.absolutePath) }
+        stagingRepositories2.getOrElse([]).each { RegularFile file -> temp.add(file.asFile.absolutePath) }
         deployer.stagingRepositories = temp.toList()
         for (ArtifactOverrideImpl artifact : artifactOverrides) {
             deployer.addArtifactOverride(artifact.toModel())
